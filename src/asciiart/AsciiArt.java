e48882fa48b5fab6748f339d65a764fb5e3a2717
Dylan Winger <20423@my4county.net>
2021-08-27 09:29:55 -0400

    Added AsciiArt Files

---
+++ src/asciiart/AsciiArt.java
@@ -0,0 +1,43 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package asciiart;
+
+/**
+ *
+ * @author dwinger
+ */
+public class AsciiArt {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+      // ASCII Art Crow From Mystery Science theater
+        
+        
+        
+        
+    System.out.println("     ,      ,    git    ");
+    System.out.println("    (\\_____/)    ");
+    System.out.println("      (_00_)   ");
+    System.out.println("        (0)      ");
+  // Head Picture
+    
+    
+    System.out.println("      __| |__   \\)");
+    System.out.println("   []/_______\\[]/ ");
+    System.out.println("   / \\______/ \\/");
+  //Body Picture
+    
+    System.out.println("  /    /__\\   ");
+    System.out.println(" (\\   /____\\  ");
+   //Legs  Picture
+    
+    }
+    
+}
